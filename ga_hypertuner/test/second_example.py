7b2eb2e49afa5a05889ca89b28cf0ac22dcdf982
Amirali <amir.om35@gmail.com>
2023-03-07 16:02:09 +0330

    changes example

--- ga_hypertuner/test/second_example.py
+++ ga_hypertuner/test/second_example.py
@@ -8,15 +8,15 @@ x_train, y_train = datasets.load_diabetes(return_X_y=True, as_frame=True)
 
 
 # setting genetic algorithm parameters
-ga_parameters = {"pop_size": 30, "fscale": 0.5, "gmax": 200, "direction": "max", "cp": 0.6}
+ga_parameters = {"pop_size": 15, "fscale": 0.6, "gmax": 200, "direction": "max", "cp": 0.6}
 
 # setting xgboost model parameters
 model_parameters = {"eta": [None, float], "min_child_weight": [None, float], "colsample_bytree": [None, float],
-                    "n_estimators": [None, int], "alpha": [None, float], "gamma": [None, float]}
+                    "n_estimators": 100, "alpha": [None, float], "gamma": [None, float]}
 
 # setting boundaries for parameters
 boundaries = {"eta": [0, 1], "min_child_weight": [0, 5], "colsample_bytree": [0, 1],
-              "alpha": [0, 1], "gamma": [0, 1], "n_estimators": [100, 1000]}
+              "alpha": [0, 1], "gamma": [0, 1]}
 
 # tuning parameters, a progress plot will be displayed every 10 generation
 best_params = Tuner.tune(x_train, y_train, xgbr, ga_parameters, model_parameters
